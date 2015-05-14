52528266322bf60bff6ca5c2964402d9cabc4cd6
Lars T Hansen <lth@acm.org>
2015-05-15 01:18:55 +0200

    Simplest possible message passing benchmark

---
+++ test/test-sendint-master.js
@@ -0,0 +1,44 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// This is the "simplest possible" message passing benchmark: it has
+// one integer location for data and uses one synchronic to ping-pong
+// a critical section for that datum between threads.
+
+// On my MacBook Pro this currently gets about 6,250,000 msgs/sec,
+// which sure beats anything possible with postMessage.
+
+var iterations = 500000;
+var bufSize = 1024;		// Should be divisible by 2 and "large enough"
+
+var w = new Worker("test-sendint-worker.js");
+var sab = new SharedArrayBuffer(bufSize);
+
+// Setup our state first.
+
+var s = new SynchronicInt32(sab, 0);
+var locIdx = 512;
+
+// Kick off the worker and wait for a message that it is ready.
+
+w.onmessage = workerReady;
+w.postMessage([sab, 0, 512, iterations], [sab]);
+
+console.log("Master waiting");
+
+function workerReady(ev) {
+    var iab = new SharedInt32Array(sab, locIdx, 1);
+    var start = Date.now();
+
+    for ( var i=0 ; i < iterations ; i++ ) {
+	iab[0]++;
+	var old = s.add(1);
+	s.expectUpdate(old+1, Number.POSITIVE_INFINITY);
+    }
+
+    var end = Date.now();
+
+    console.log("Should be " + iterations*2 + ": " + iab[0]);
+    console.log(Math.round(1000 * (2*iterations) / (end - start)) + " messages/s");
+}
---
+++ test/test-sendint-worker.js
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+importScripts("../src/synchronic.js");
+
+onmessage =
+    function (ev) {
+	var [sab, sabIdx, locIdx, iterations] = ev.data;
+
+	// Initialize our state
+
+	var s = new SynchronicInt32(sab, 0);
+	var iab = new SharedInt32Array(sab, locIdx, 1);
+
+	// Let the master know we're ready to go
+
+	postMessage("ready");
+
+	var x = 0;
+	for ( var i=0 ; i < iterations ; i++ ) {
+	    s.expectUpdate(x, Number.POSITIVE_INFINITY);
+	    iab[0]++;
+	    x = s.add(1)+1;
+	}
+
+	console.log("Worker exiting");
+    };
