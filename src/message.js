adf580e0681390a80063698c1c571311f0149a7f
Lars T Hansen <lth@acm.org>
2016-03-07 19:35:55 +0100

    Cleanup

--- src/message.js
+++ src/message.js
@@ -1,9 +1,14 @@
+// If the message is an array whose first element is a string that
+// matches "tag" then consume the message and invoke fn on its data.
+
 function dispatchMessage(target, tag, fn) {
-    target.addEventListener("message",
-			    function (ev) {
-				if (Array.isArray(ev.data) && ev.data.length >= 1 && ev.data[0] == tag) {
-				    ev.stopImmediatePropagation();
-				    fn(ev.data);
-				}
-			    });
+    if (typeof tag != "string")
+	throw new Error("Tag to dispatchMessage must be string: " + tag);
+
+    target.addEventListener("message", function (ev) {
+	if (Array.isArray(ev.data) && ev.data.length >= 1 && ev.data[0] === tag) {
+	    ev.stopImmediatePropagation();
+	    fn(ev.data);
+	}
+    });
 }
