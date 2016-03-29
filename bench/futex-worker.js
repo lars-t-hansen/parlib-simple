c4b4f306fe157d6a738ac4690b6d318f881c82ec
Lars T Hansen <lth@acm.org>
2016-03-29 07:28:03 -0700

    Adapt to use Atomics.pause if available

--- bench/futex-worker.js
+++ bench/futex-worker.js
@@ -8,7 +8,7 @@ onmessage =
     function (ev) {
 	var [sab, sabIdx, locIdx, iterations, who] = ev.data;
 
-	var s = new Int32Array(sab, sabIdx, 1);	  // "sync" location
+	var s = new Int32Array(sab, sabIdx, 2);	  // "sync" locations (signal, counter)
 	var iab = new Int32Array(sab, locIdx, 1); // "work" location
 
 	var start = Date.now();
@@ -17,19 +17,15 @@ onmessage =
 	var x = 0;
 	for ( var i=0 ; i < iterations ; i++ ) {
 	    if (who == 0) {
-		while (Atomics.load(s, 0) == x)
-		    Atomics.futexWait(s, 0, x);
+		expectUpdate(s, 0, x);
 		x++;
 		iab[0]++;
-		Atomics.store(s, 0, ++x);
-		Atomics.futexWake(s, 0, 1);
+		storeNotify(s, 0, ++x);
 	    }
 	    else {
 		iab[0]++;
-		Atomics.store(s, 0, ++x);
-		Atomics.futexWake(s, 0, 1);
-		while (Atomics.load(s, 0) == x)
-		    Atomics.futexWait(s, 0, x);
+		storeNotify(s, 0, ++x);
+		expectUpdate(s, 0, x);
 		x++;
 	    }
 	}
@@ -43,3 +39,40 @@ onmessage =
 
 	msg("Worker " + who + " exiting");
     };
+
+function expectUpdate(s, idx, current) {
+    if (spinWait(s, idx, current))
+	return;
+    while (Atomics.load(s, idx) == current) {
+	// These counters are *hugely* important for performance when
+	// using Atomics.pause(), which might indicate that the futex
+	// implementation needs some work!  The counter radically
+	// reduces the number of calls to futexWake().  It makes sense
+	// that it should be faster not to call futexWake, since most
+	// of the time that call is not necessary - the spinning was
+	// enough.  Even so, can we do something?  Can we add this
+	// accounting to the futex system, for example?
+	Atomics.add(s, idx+1, 1);
+	Atomics.futexWait(s, idx, current);
+	Atomics.sub(s, idx+1, 1);
+    }
+}
+
+function storeNotify(s, idx, value) {
+    Atomics.store(s, idx, value);
+    if (Atomics.load(s, idx+1))
+	Atomics.futexWake(s, idx, 1);
+}
+
+function spinWaitPause(s, idx, current) {
+    for (let i=0 ;; i++ ) {
+	if (Atomics.load(s, idx) != current)
+	    return true;
+	if (!Atomics.pause(i))
+	    return false;
+    }
+}
+
+function spinWaitNoPause(s, idx, current) {}
+
+var spinWait = (Atomics.pause ? spinWaitPause : spinWaitNoPause);
